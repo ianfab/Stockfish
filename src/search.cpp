e915cb5adcb6ce553453b0a5a964a0f22b549cea
ianfab <ianfab@users.noreply.github.com>
2017-07-01 23:36:22 +0200

    Less move count pruning in atomic chess

--- src/search.cpp
+++ src/search.cpp
@@ -306,6 +306,15 @@ void Search::init() {
 
   for (Variant var = CHESS_VARIANT; var < VARIANT_NB; ++var)
   {
+#ifdef ATOMIC
+  if (var == ATOMIC_VARIANT)
+      for (int d = 0; d < 16; ++d)
+      {
+          FutilityMoveCounts[var][0][d] = int(3.5 + 0.80 * pow(d, 1.80));
+          FutilityMoveCounts[var][1][d] = int(7.0 + 1.00 * pow(d, 2.00));
+      }
+  else
+#endif
 #ifdef CRAZYHOUSE
   if (var == CRAZYHOUSE_VARIANT)
       for (int d = 0; d < 16; ++d)
