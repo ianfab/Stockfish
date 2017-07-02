b5500e298e42be0f809ca330afb97ac4bf594163
ianfab <ianfab@users.noreply.github.com>
2017-07-02 12:42:14 +0200

    More aggressive move count pruning for horde chess

--- src/search.cpp
+++ src/search.cpp
@@ -315,6 +315,15 @@ void Search::init() {
       }
   else
 #endif
+#ifdef HORDE
+  if (var == HORDE_VARIANT)
+      for (int d = 0; d < 16; ++d)
+      {
+          FutilityMoveCounts[var][0][d] = int(1.5 + 0.5 * pow(d, 1.50));
+          FutilityMoveCounts[var][1][d] = int(4.0 + 0.7 * pow(d, 2.00));
+      }
+  else
+#endif
 #ifdef RACE
   if (var == RACE_VARIANT)
       for (int d = 0; d < 16; ++d)
