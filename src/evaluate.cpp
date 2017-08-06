152f4863a489a499ec3aaca30c74fcf4cb69cacf
ianfab <ianfab@users.noreply.github.com>
2017-08-06 18:19:43 +0200

    Do not consider queen xrays with rook/bishop

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -299,10 +299,9 @@ namespace {
     while ((s = *pl++) != SQ_NONE)
     {
         // Find attacked squares, including x-ray attacks
-        b = Pt == BISHOP ?    attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, QUEEN))
-          : Pt ==   ROOK ?    attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK, QUEEN))
-          : Pt ==  QUEEN ? (  attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK, QUEEN))
-                            | attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, BISHOP, QUEEN)))
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, QUEEN))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK, QUEEN))
+          : Pt ==  QUEEN ? attacks_bb< QUEEN>(s, pos.pieces() ^ pos.pieces(Us, QUEEN))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.pinned_pieces(Us) & s)
