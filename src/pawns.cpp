5fc3b13552c473fb7d7b0945899fea74824ebb4e
ianfab <ianfab@users.noreply.github.com>
2017-08-07 22:31:53 +0200

    Consider blocked pawns

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -190,7 +190,7 @@ namespace {
         if (lever)
             score += Lever[relative_rank(Us, s)];
 
-        if (leverPush)
+        if (leverPush && !(theirPawns & (s + Up)))
             leverCount++;
     }
 
