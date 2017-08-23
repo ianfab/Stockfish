06b10a2014326161b01ece3db8a229f287dbd34e
ianfab <ianfab@users.noreply.github.com>
2017-08-23 21:53:12 +0200

    Tune ShelterWeakness

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -51,7 +51,7 @@ namespace {
 
   // Weakness of our pawn shelter in front of the king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawns or our pawn is behind our king.
-  const Value ShelterWeakness[][int(FILE_NB) / 2][RANK_NB] = {
+  Value ShelterWeakness[][int(FILE_NB) / 2][RANK_NB] = {
     { { V(100), V(20), V(10), V(46), V(82), V( 86), V( 98) },
       { V(116), V( 4), V(28), V(87), V(94), V(108), V(104) },
       { V(109), V( 1), V(59), V(87), V(62), V( 91), V(116) },
@@ -61,6 +61,7 @@ namespace {
       { V(109), V( 1), V(59), V(87), V(62), V( 91), V(116) },
       { V( 75), V(12), V(43), V(59), V(90), V( 84), V(112) } }
   };
+  TUNE(SetRange(0, 300), ShelterWeakness);
 
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank].
   // For the unopposed and unblocked cases, RANK_1 = 0 is used when opponent has no pawn
