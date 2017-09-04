3e7a918c03eb491cb0396ec3cef77db9fa84c61f
ianfab <ianfab@users.noreply.github.com>
2017-09-04 23:00:36 +0200

    king attack weight for pawns

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -233,7 +233,7 @@ namespace {
   #undef V
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 78, 56, 45, 11 };
+  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 5, 78, 56, 45, 11 };
 
   // Penalties for enemy's safe checks
   const int QueenSafeCheck  = 780;
@@ -279,7 +279,8 @@ namespace {
             kingRing[Us] |= shift<Up>(b);
 
         kingAttackersCount[Them] = popcount(b & pe->pawn_attacks(Them));
-        kingAdjacentZoneAttacksCount[Them] = kingAttackersWeight[Them] = 0;
+        kingAttackersWeight[Them] = kingAttackersCount[Them] * KingAttackWeights[PAWN];
+        kingAdjacentZoneAttacksCount[Them] = 0;
     }
     else
         kingRing[Us] = kingAttackersCount[Them] = 0;
