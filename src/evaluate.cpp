990ed5df616f81c00040b873014d87e45486879c
ianfab <ianfab@users.noreply.github.com>
2017-09-07 08:16:20 +0200

    tweak king attack weights

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -222,7 +222,7 @@ namespace {
   #undef V
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 5, 78, 56, 45, 11 };
+  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 2, 84, 60, 46,  9 };
 
   // Penalties for enemy's safe checks
   const int QueenCheck  = 780;
