df84a61c7a3f236ec7303b0ca7876c2c6e897afb
ianfab <ianfab@users.noreply.github.com>
2017-09-23 14:10:27 +0200

    simplify move ordering

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -161,11 +161,7 @@ void MovePicker::score() {
                    + (*contHistory[3])[pos.moved_piece(m)][to_sq(m)];
 #ifdef ANTI
           if (pos.is_anti() && pos.attackers_to(to_sq(m), pos.pieces() ^ from_sq(m)) & pos.pieces(~pos.side_to_move()))
-          {
               m.value += (1 << 28);
-              if (!(pos.attackers_to(from_sq(m)) & pos.pieces(~pos.side_to_move())))
-                  m.value += (1 << 27);
-          }
 #endif
       }
 
