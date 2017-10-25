7322a7e731add57737f0cfcb3ba8c0ea4f0df50c
ianfab <ianfab@users.noreply.github.com>
2017-10-25 23:10:52 +0200

    consider pawn count in center in initiative

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1433,6 +1433,10 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+#ifdef KOTH
+    if (pos.is_koth())
+        initiative += 8 * (2 - popcount(Center & pos.pieces(PAWN)));
+#endif
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
