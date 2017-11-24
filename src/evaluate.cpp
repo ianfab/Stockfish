888932b53074da58f44e6f9f9d2e6b45e28b50b8
ianfab <ianfab@users.noreply.github.com>
2017-11-24 16:14:11 +0100

    No drawish scale factor for opposite-colored bishops in racing kings

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1572,6 +1572,9 @@ namespace {
 #ifdef ATOMIC
     if (pos.is_atomic()) {} else
 #endif
+#ifdef RACE
+    if (pos.is_race()) {} else
+#endif
     if (sf == SCALE_FACTOR_NORMAL || sf == SCALE_FACTOR_ONEPAWN)
     {
         if (pos.opposite_bishops())
