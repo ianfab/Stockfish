13431713e579556677d055d93aa51bb76a7ce570
ianfab <ianfab@users.noreply.github.com>
2018-01-10 22:05:30 +0100

    tweak breakthrough formula

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1335,7 +1335,7 @@ namespace {
                     int pawnsl = f > FILE_A ? std::min(popcount(pos.pieces(Them, PAWN) & FileBB[f - 1]), pawns) : 0;
                     int pawnsr = f < FILE_H ? std::min(popcount(pos.pieces(Them, PAWN) & FileBB[f + 1]), pawns) : 0;
                     int attackers = pawns ? popcount(pos.attackers_to(frontmost_sq(Them, pos.pieces(Them, PAWN) & file_bb(f))) & pos.pieces(Us)) : 0;
-                    min = std::max(std::min(min, 2 * (pawnsl + pawnsr) - attackers), 0);
+                    min = std::max(std::min(min, 2 * (pawnsl + pawnsr) - attackers + 1), 0);
                 }
             }
             score += make_score(71, 61) * pos.count<PAWN>(Them) / (2 + min) / (pos.pieces(Us, QUEEN) ? 1 : 2);
