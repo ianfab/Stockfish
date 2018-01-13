05fc9fa7ed9fd613c60f8bfb9ddec5a004cd28cf
ianfab <ianfab@users.noreply.github.com>
2018-01-13 22:51:20 +0100

    increase weight for pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -233,7 +233,7 @@ namespace {
   #undef V
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 5, 78, 56, 45, 11 };
+  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 8, 78, 56, 45, 11 };
 
   // Penalties for enemy's safe checks
   const int QueenSafeCheck  = 780;
