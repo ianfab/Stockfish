bc4cc42d042280884da3d9e2f718c33b1b963680
ianfab <ianfab@users.noreply.github.com>
2018-01-14 13:21:11 +0100

    extend to higher depths

--- src/search.cpp
+++ src/search.cpp
@@ -69,7 +69,7 @@ namespace {
   // Razoring and futility margin based on depth
   // razor_margin[0] is unused as long as depth >= ONE_PLY in search
   const int razor_margin[] = { 0, 570, 603, 554 };
-  const int futility_margin[] = { 0, 140, 300, 445, 585, 748, 908 };
+  const int futility_margin[] = { 0, 140, 300, 445, 585, 748, 908, 1100, 1300};
 
   // Futility and reductions lookup tables, initialized at startup
   int FutilityMoveCounts[2][16]; // [improving][depth]
@@ -667,7 +667,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (skipped when in check)
     if (   !rootNode
-        &&  depth < 7 * ONE_PLY
+        &&  depth < 9 * ONE_PLY
         &&  eval - futility_margin[depth / ONE_PLY] >= beta
         &&  eval < VALUE_KNOWN_WIN)  // Do not return unproven wins
         return eval;
