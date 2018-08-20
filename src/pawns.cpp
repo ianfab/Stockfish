edd2f483b39a8b4bda9e3083e91fad03bfe18b8c
ianfab <ianfab@users.noreply.github.com>
2018-08-20 22:58:52 +0200

    exclude distant doubled pawns
    bench: 4625585

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -103,7 +103,7 @@ namespace {
         stoppers   = theirPawns & passed_pawn_mask(Us, s);
         lever      = theirPawns & PawnAttacks[Us][s];
         leverPush  = theirPawns & PawnAttacks[Us][s + Up];
-        doubled    = (ourPawns ^ s) & file_bb(s);
+        doubled    = ourPawns   & (SquareBB[s - Up] | (s + Up));
         neighbours = ourPawns   & adjacent_files_bb(f);
         phalanx    = neighbours & rank_bb(s);
         supported  = neighbours & rank_bb(s - Up);
