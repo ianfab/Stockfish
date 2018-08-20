4eb19f554343c3d873e669ae3f160a834fb55408
ianfab <ianfab@users.noreply.github.com>
2018-08-20 21:36:58 +0200

    Score doubled pawns individually
    Consequences:
    - Penalty for distant doubled pawns.
    - n-upled pawns get penalty n times instead of 2*(n-1).
    - Doubled pawns are checked for supporting pawns individually.
    
    bench: 4641213

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward = S( 9, 24);
-  constexpr Score Doubled  = S(11, 56);
+  constexpr Score Doubled  = S( 5, 28);
   constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
@@ -103,7 +103,7 @@ namespace {
         stoppers   = theirPawns & passed_pawn_mask(Us, s);
         lever      = theirPawns & PawnAttacks[Us][s];
         leverPush  = theirPawns & PawnAttacks[Us][s + Up];
-        doubled    = ourPawns   & (s - Up);
+        doubled    = ourPawns   & file_bb(s);
         neighbours = ourPawns   & adjacent_files_bb(f);
         phalanx    = neighbours & rank_bb(s);
         supported  = neighbours & rank_bb(s - Up);
