f4dce7d381b9053f217fb53f50cd6b98840942db
ianfab <ianfab@users.noreply.github.com>
2018-08-22 08:52:54 +0200

    Modify doubled pawn condition
    bench: 4694505

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -141,7 +141,7 @@ namespace {
         else if (backward)
             score -= Backward, e->weakUnopposed[Us] += !opposed;
 
-        if (doubled && !supported)
+        if (doubled && !supported && !phalanx && !lever)
             score -= Doubled;
     }
 
