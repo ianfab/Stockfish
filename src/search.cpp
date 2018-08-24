1d4a26466f4d38ca40eaf11012879a513d51cabe
ianfab <ianfab@users.noreply.github.com>
2018-08-24 21:36:54 +0200

    tweak SEE pruning formula
    bench: 4451297

--- src/search.cpp
+++ src/search.cpp
@@ -963,7 +963,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (   !extension // (~20 Elo)
-                   && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
+                   && !pos.see_ge(move, Value(-150 * (depth / ONE_PLY) - 10 * (depth / ONE_PLY) * (depth / ONE_PLY))))
                   continue;
       }
 
