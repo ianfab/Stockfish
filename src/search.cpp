d46b9f6a614cfb81425dd248664ab67b59f94f48
ianfab <ianfab@users.noreply.github.com>
2018-08-25 01:35:49 +0200

    tweak stat bonus formula - denom 200
    bench: 4401608

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return 10000 * d * d / (100 + d * d);
+    return 10000 * d * d / (200 + d * d);
   }
 
   // Skill structure is used to implement strength limit
