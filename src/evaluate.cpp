812ac391a1256599dc147df275ac55feb39776fe
Fabian Fichter <ianfab@users.noreply.github.com>
2018-09-01 15:55:40 +0200

    quadratic initiative formula - take 2
    bench: 4207783

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -766,8 +766,9 @@ namespace {
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
-                    + 48 * !pos.non_pawn_material()
-                    -118 ;
+                    + 48 * !pos.non_pawn_material();
+
+    complexity = complexity * complexity / 196 - 99;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
