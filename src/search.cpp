ee410eb000f091486a4d7e7daf9b815b9683dbe3
Fabian Fichter <ianfab@users.noreply.github.com>
2018-09-06 08:36:40 +0200

    tweak singular extension threshold
    bench: 4980813

--- src/search.cpp
+++ src/search.cpp
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
+          Value rBeta = std::max(ttValue - 3 * depth / (2 * ONE_PLY), -VALUE_MATE);
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, depth / 2, cutNode);
           ss->excludedMove = MOVE_NONE;
